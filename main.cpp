9ec34bf1752de596cabb43fb980b19152de1451e
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-07-10 18:28:40 -0500

    Prepara el main para comenzar el dasarrollo del sistema

--- main.cpp
+++ main.cpp
@@ -1,31 +1,7 @@
 #include<iostream>
 #include<string>
+#include "structs.h"
 using namespace std;
-
-struct Operacion {
-    int idOperacion;
-    string canal;
-    string tipo;    
-};
-
-struct Movimiento {
-    float monto;  
-    Operacion operacion;
-};
-
-struct Cuenta {
-    float saldo;
-    int numeroCuenta;
-    Movimiento movimientos[100];
-    int cantidadMovimientos;
-};
-
-struct Persona {
-    int dni;
-    string nombre;
-    Cuenta cuentas[100];
-    int cantidadCuentas;
-};
 
 int main() {
     
