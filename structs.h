73526596254e5d706bf2144c8065e9ac509c07ae
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-07-10 19:04:02 -0500

    Agrega structs base del sistema bancario: Persona, Cuenta, Movimiento y Operacion

---
+++ structs.h
@@ -0,0 +1,32 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <string>
+using namespace std;
+
+struct Operacion {
+    int idOperacion;
+    string canal;
+    string tipo;    
+};
+
+struct Movimiento {
+    float monto;  
+    Operacion operacion;
+};
+
+struct Cuenta {
+    float saldo;
+    int numeroCuenta;
+    Movimiento movimientos[100];
+    int cantidadMovimientos;
+};
+
+struct Persona {
+    int dni;
+    string nombre;
+    Cuenta cuentas[100];
+    int cantidadCuentas;
+};
+
+#endif
