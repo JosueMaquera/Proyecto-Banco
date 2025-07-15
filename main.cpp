ba9032538f85459fdf23d04278d68be8911873cf
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-07-14 23:22:45 -0500

    Agrega la funcion gestionarMovimiento

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int buscarPersona(string dni);
 void registrarPersona();
 void agregarCuenta();
 void mostrarPersona();
+void gestionarMovimiento();
 
 Persona personas[100];
 int totalPersonas = 0;
@@ -104,4 +105,66 @@ void mostrarPersona() {
     for (int i = 0; i < p.cantidadCuentas; i++) {
         cout << "  Cuenta " << i + 1 << ": " << p.cuentas[i].numeroCuenta << " | Saldo: " << p.cuentas[i].saldo << endl;
     }
+}
+
+void gestionarMovimiento() {
+    string dni;
+    cout << "DNI de la persona: ";
+    cin >> dni;
+
+    int indicePersona = buscarPersona(dni);
+    if (indicePersona == -1) {
+        cout << "Persona no encontrada." << endl;
+        return;
+    }
+
+    Persona& p = personas[indicePersona];
+
+    if (p.cantidadCuentas == 0) {
+        cout << "Esta persona no tiene cuentas registradas." << endl;
+        return;
+    }
+
+    cout << "Seleccione una cuenta: " << endl;
+    for (int i = 0; i < p.cantidadCuentas; i++) {
+        cout << i + 1 << ". " << p.cuentas[i].numeroCuenta << endl;
+    }
+
+    int indiceCuenta;
+    cout << "Ingrese opcion";
+    cin >> indiceCuenta;
+    indiceCuenta -= 1;
+
+    if (indiceCuenta < 0 || indiceCuenta >= p.cantidadCuentas) {
+        cout << "Cuenta invalida." << endl;
+        return;
+    }
+
+    Cuenta& cuenta = p.cuentas[indiceCuenta];
+
+    Movimiento nuevo;
+    
+    cout << "Ingrese tipo (por ejemplo: deposito, retiro): ";
+    getline(cin, nuevo.operacion.tipo);
+
+    cout << "Ingrese el monto del " << nuevo.operacion.tipo << ":";
+    cin >> nuevo.monto;
+
+    cout << "Ingrese canal (por ejemplo: app, ventanilla): ";
+    getline(cin, nuevo.operacion.canal);
+
+    if (nuevo.operacion.tipo == "deposito") {
+        cuenta.saldo += nuevo.monto;
+    } else if (nuevo.operacion.tipo == "retiro") {
+        if (nuevo.monto > cuenta.saldo) {
+            cout << "Saldo insuficiente." << endl;
+        }
+        cuenta.saldo -= nuevo.monto;
+    } else {
+        cout << "Tipo de operacion no valida." << endl;
+        return;
+    }
+
+    cuenta.movimientos[cuenta.cantidadMovimientos += 1] = nuevo;
+    cout << "Movimiento registrado correctamente." << endl;
 }
