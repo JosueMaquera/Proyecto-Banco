a66af3a28b36ae7631bb0effd09d16b4af6b793b
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-07-23 18:24:31 -0500

    Modulariza las funciones persona

---
+++ persona.h
@@ -0,0 +1,12 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <string>
+using namespace std;
+
+bool Existedni(string dni);
+int buscarPersona(string dni);
+void registrarPersona();
+void mostrarPersona();
+
+#endif
